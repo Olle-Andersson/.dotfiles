729e45f8a9e05ccac152677c73a3cd234ace1d71
Olov Andersson <olov.andersson@pm.me>
2026-06-13 19:33:25 +0200

    Modified colortheme

--- nvim/plugin/srcery.lua
+++ nvim/plugin/srcery.lua
@@ -1,3 +1,16 @@
 vim.pack.add({ 'https://github.com/srcery-colors/srcery-vim' })
 
+vim.g.srcery_background = "#0e0e0e"
+vim.g.srcery_bright_white= "#ffffff"
+vim.g.srcery_green= "#2ab028"
+vim.g.srcery_bright_green= "#8bca37"
+vim.g.srcery_white= "#c4c4c4"
+vim.g.srcery_grey2= "#242424"
+vim.g.srcery_grey3= "#2f2f2f"
+vim.g.srcery_grey4= "#383838"
+vim.g.srcery_grey5= "#414141"
+vim.g.srcery_grey6= "#4d4d4d"
+
+
+
 vim.cmd.colorscheme('srcery')
